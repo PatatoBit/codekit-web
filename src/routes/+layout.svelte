638f986d0a8f7874b99ae88fb89b91cd6b9fd6b1
Patato <pathonscript@gmail.com>
2025-03-22 09:43:25 +0700

    Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../styles/global.scss';
+	import '../styles/variables.scss';
+	import '../styles/fonts.scss';
+
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+{@render children()}
+
+<style lang="scss">
+	@use '../styles/variables.scss';
+	@use '../styles/fonts.scss';
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<main class="page fullcenter">
+	<h1>Hello World</h1>
+
+	<p>
+		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis dignissimos nam magni delectus
+		necessitatibus dolorem, veniam labore commodi, suscipit adipisci explicabo vitae perferendis
+		praesentium omnis exercitationem blanditiis numquam fugit? Maxime?
+	</p>
+</main>
+
+<style lang="scss">
+</style>
