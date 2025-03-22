e8fb1fd36945d7f252ebbc8f51c61c8ba1bc8c09
Patato <pathonscript@gmail.com>
2025-03-22 11:10:01 +0700

    statements layout from hero

---
+++ src/components/Views/Statements.svelte
@@ -0,0 +1,38 @@
+<section class="wrapper fullcenter hero-main">
+	<div class="content">
+		<h1>The newest business analytics platform</h1>
+
+		<p>
+			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+			laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+			beatae vitae dicta sunt explicabo.
+		</p>
+
+		<div class="flex flex-row gap-3 mt-5">
+			<button class="btn btn-primary">Learn more</button>
+		</div>
+	</div>
+
+	<div class="image">
+		<img src="/002_illustration.jpg" alt="Illustration" />
+	</div>
+</section>
+
+<section class="wrapper fullcenter hero-main">
+	<div class="image">
+		<img src="/003_illustration.jpg" alt="Illustration" />
+	</div>
+	<div class="content">
+		<h1>Radically new data solutions</h1>
+
+		<p>
+			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+			laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+			beatae vitae dicta sunt explicabo.
+		</p>
+
+		<div class="flex flex-row gap-3 mt-5">
+			<button class="btn btn-primary">Learn more</button>
+		</div>
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Hero from '../components/Views/Hero.svelte';
 	import Features from '../components/Views/Features.svelte';
+	import Statements from '../components/Views/Statements.svelte';
 </script>
 
 <Hero />
 <Features />
+<Statements />
